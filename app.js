52ae68979168a0825e730e09db3400dc59efe899
whitehorse-dev <whitehorse.dev.sp@gmail.com>
2022-08-31 16:18:25 +0530

    started creating views(html) to make real websites.

--- app.js
+++ app.js
@@ -11,4 +11,9 @@ app.use(bodyParser.urlencoded({ entended: false }))
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
+// Handling the 404 error pages
+app.use('/', (req, res, next) => {
+  res.status(404).send('<h1> Sorry!! This page was not found!! </h1>')
+})
+
 app.listen(3000)
